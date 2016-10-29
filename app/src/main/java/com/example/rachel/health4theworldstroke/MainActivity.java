843ec044b574dcc56bb45351daa468ca40da24ab
rachelgrau <rgrau@stanford.edu>
2016-10-29 11:22:37 -0300

    First commit

---
+++ app/src/main/java/com/example/rachel/health4theworldstroke/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.rachel.health4theworldstroke;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.health4theworldstroke.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void helpMeSpeakSelected(View view) {
+        Intent intent = new Intent(this, HelpMeSpeakActivity.class);
+//        intent.putExtra(EXTRA_MESSAGE, "Help Me Speak!");
+        startActivity(intent);
+    }
+
+    public void learnSelected(View view) {
+        Intent intent = new Intent(this, LearnActivity.class);
+        startActivity(intent);
+    }
+
+    public void remindersSelected(View view) {
+        Intent intent = new Intent(this, RemindersActivity.class);
+        startActivity(intent);
+    }
+}
