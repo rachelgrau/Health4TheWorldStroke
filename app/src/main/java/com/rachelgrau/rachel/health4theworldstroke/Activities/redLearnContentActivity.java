7c5cfc33f30871646273e43db7914f8496fa63c4
rachelgrau <rgrau@stanford.edu>
2017-02-08 11:33:13 -0300

    Bold only certain words

--- app/src/main/java/com/rachelgrau/rachel/health4theworldstroke/Activities/redLearnContentActivity.java
+++ app/src/main/java/com/rachelgrau/rachel/health4theworldstroke/Activities/redLearnContentActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -53,34 +54,81 @@ public class RedLearnContentActivity extends AppCompatActivity {
     public void setUpBloodSugar() {
         addHeaderWithText("PROBLEM");
         addLineBreak();
-        String firstPar = "<b>" + "High blood sugar " + "</b>" + "can increase the risk of developing strokes and heart disease. A blood test called HbA1c gives a picture of your average blood sugar control for the past 2 to 3 months.";
-        addTextViewWithText(firstPar);
+        /* FIRST PARAGRAPH */
+        String firstPart = "High blood sugar ";
+        String secondPart = "can increase the risk of developing strokes and heart disease. A blood test called HbA1c gives a picture of your average blood sugar control for the past 2 to 3 months.";
+        Spannable firstParSpannable = new SpannableString(firstPart+secondPart);
+        firstParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf")), 0, firstPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        firstParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), firstPart.length(), firstPart.length() + secondPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        addTextViewWithText(firstParSpannable);
+
         addHeaderWithText("WHAT TO DO");
         addLineBreak();
-        String secondPar = "The American Heart Association recommends keeping blood sugar levels (Hgb1A1c)" + "<b>" + " below 6% " + "</b>" + "for patients with pre-diabetes and" + "<b>" + " below 7% " + "</b>" + "for diabetic patients.";
-        addTextViewWithText(secondPar);
+        /* SECOND PARAGRAPH */
+        firstPart = "The American Heart Association recommends keeping blood sugar levels (Hgb1A1c)";
+        secondPart = " below 6% ";
+        String thirdPart = "for patients with pre-diabetes and";
+        String fourthPart = " below 7% ";
+        String fifthPart = "for diabetic patients.";
+        Spannable secondParSpannable = new SpannableString(firstPart + secondPart + thirdPart + fourthPart + fifthPart);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), 0, firstPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf")), firstPart.length(), firstPart.length() + secondPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), firstPart.length() + secondPart.length(), firstPart.length() + secondPart.length() + thirdPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf")), firstPart.length() + secondPart.length() + thirdPart.length(), firstPart.length() + secondPart.length() + thirdPart.length() + fourthPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), firstPart.length() + secondPart.length() + thirdPart.length() + fourthPart.length(), firstPart.length() + secondPart.length() + thirdPart.length() + fourthPart.length() + fifthPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        addTextViewWithText(secondParSpannable);
     }
 
     public void setUpBloodPressure() {
         addHeaderWithText("PROBLEM");
         addLineBreak();
-        String firstPar = "<b>" + "High blood pressure" + "</b>" + " can cause strokes by damaging arteries throughout the body causing them to burst or clog more easily.";
-        addTextViewWithText(firstPar);
+        /* FIRST PARAGRAPH */
+        String firstPart = "High blood pressure";
+        String secondPart = " can cause strokes by damaging arteries throughout the body causing them to burst or clog more easily.";
+        Spannable firstParSpannable = new SpannableString(firstPart+secondPart);
+        firstParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf")), 0, firstPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        firstParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), firstPart.length(), firstPart.length() + secondPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        addTextViewWithText(firstParSpannable);
+
         addHeaderWithText("WHAT TO DO");
         addLineBreak();
-        String secondPar = "In the general population aged 60 years or older, treating high BP to a goal of" + "<b>" + " lower than 150/90 mm Hg " + "</b>" + "reduces stroke.";
-        addTextViewWithText(secondPar);
+        /* SECOND PARAGRAPH */
+        firstPart = "In the general population aged 60 years or older, treating high BP to a goal of";
+        secondPart = " lower than 150/90 mm Hg ";
+        String thirdPart = "reduces stroke.";
+        Spannable secondParSpannable = new SpannableString(firstPart + secondPart + thirdPart);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), 0, firstPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf")), firstPart.length(), firstPart.length() + secondPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), firstPart.length() + secondPart.length(), firstPart.length() + secondPart.length() + thirdPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        addTextViewWithText(secondParSpannable);
     }
 
     public void setUpHeartRate() {
         addHeaderWithText("PROBLEM");
         addLineBreak();
-        String firstPar = "<b>" + "Atrial Fibrillation " + "</b>" + ", or irregular heart rate, is a risk factor for stroke.";
-        addTextViewWithText(firstPar);
+        /* FIRST PARAGRAPH */
+        String firstWord = "Atrial Fibrillation";
+        String secondWord = ", or irregular heart rate, is a risk factor for stroke.";
+        Spannable firstParSpannable = new SpannableString(firstWord+secondWord);
+        firstParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf")), 0, firstWord.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        firstParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), firstWord.length(), firstWord.length() + secondWord.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        addTextViewWithText(firstParSpannable);
+
         addHeaderWithText("WHAT TO DO");
         addLineBreak();
-        String secondPar = "If you have suspicion of an irregular heart rate, contact your doctor immediately for testing, which might include" + "<b>" + " electrocardiogram (ECG/EKG)" + "</b>" + ". Based on your past medical history and your risk for having a future stroke, there are several options for medications, like" + "<b>" + " anticoagulants" + "</b>" + ", that will help manage AFib.";
-        addTextViewWithText(secondPar);
+        /* SECOND PARAGRAPH */
+        String firstPart = "If you have suspicion of an irregular heart rate, contact your doctor immediately for testing, which might include";
+        String secondPart = " electrocardiogram (ECG/EKG)"; // bold
+        String thirdPart = ". Based on your past medical history and your risk for having a future stroke, there are several options for medications, like";
+        String fourthPart = " anticoagulants"; // bold
+        String fifthPart = ", that will help manage AFib.";
+        Spannable secondParSpannable = new SpannableString(firstPart + secondPart + thirdPart + fourthPart + fifthPart);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), 0, firstPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf")), firstPart.length(), firstPart.length() + secondPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), firstPart.length() + secondPart.length(), firstPart.length() + secondPart.length() + thirdPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf")), firstPart.length() + secondPart.length() + thirdPart.length(), firstPart.length() + secondPart.length() + thirdPart.length() + fourthPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        secondParSpannable.setSpan( new CustomTypefaceSpan("sans-serif",Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf")), firstPart.length() + secondPart.length() + thirdPart.length() + fourthPart.length(), firstPart.length() + secondPart.length() + thirdPart.length() + fourthPart.length() + fifthPart.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        addTextViewWithText(secondParSpannable);
     }
 
     public void addHeaderWithText(String text) {
@@ -95,12 +143,12 @@ public class RedLearnContentActivity extends AppCompatActivity {
     }
 
     /* Text should be in HTML */
-    public void addTextViewWithText(String text) {
+    public void addTextViewWithText(Spannable text) {
         LinearLayout ll = (LinearLayout) findViewById(R.id.content);
         TextView secondPar = new TextView(this);
         Typeface font = Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf");
         secondPar.setTypeface(font);
-        secondPar.setText(Html.fromHtml(text));
+        secondPar.setText(text);
         secondPar.setPadding(75, 10, 75, 0);
         secondPar.setTextSize(18);
         secondPar.setTextColor(Color.WHITE);
